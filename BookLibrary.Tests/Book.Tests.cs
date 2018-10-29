92d1b9fb9277e02fc799f53f76c9e36bd3f836ad
Denis Lukayenka <denis.lukayenka@gmail.com>
2018-10-29 09:41:44 +0300

    Task 1

--- BookLibrary.Tests/Book.Tests.cs
+++ BookLibrary.Tests/Book.Tests.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using static BookLibrary.Book;
 
 namespace BookLibrary.Tests
 {
+    [TestFixture]
     public class Class1
     {
+        private Book book = new Book()
+        {
+            Title = "C# in Depth",
+            Author = "Jon Skeet",
+            Year = 2019,
+            PublishingHouse = "Manning",
+            Edition = 4,
+            Pages = 900,
+            Price = 40
+        };
+
+        [TestCase("A", ExpectedResult = "Book record: Jon Skeet, C# in Depth, 2019, Manning")]
+        [TestCase("B", ExpectedResult = "Book record: Jon Skeet, C# in Depth, 2019")]
+        [TestCase("C", ExpectedResult = "Book record: Jon Skeet, C# in Depth")]
+        [TestCase("D", ExpectedResult = "Book record: C# in Depth, 2019, Manning")]
+        [TestCase("E", ExpectedResult = "Book record: C# in Depth, Manning")]
+        [TestCase("F", ExpectedResult = "Book record: Jon Skeet, C# in Depth, 2019, Manning, ed.4, p.900, 40,00 ₽")]
+        [TestCase("B", ExpectedResult = "Book record: Jon Skeet, C# in Depth, 2019")]
+        public string ToString_FormatsWithoutCultureInfo(string format)
+            => book.ToString(format);
     }
+    //- Book record: Jon Skeet, C# in Depth, 2019, "Manning", 
+    //- Book record: Jon Skeet, C# in Depth, 2019
+    //- Book record: Jon Skeet, C# in Depth
+    //- Book record: C# in Depth, 2019, "Manning"
+    //- Book record: C# in Depth и т.д.
 }
--- BookLibrary/Book.cs
+++ BookLibrary/Book.cs
@@ -1,13 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BookLibrary
 {
-    public class Book
+    /// <summary>
+    /// Model of book.
+    /// </summary>
+    public class Book : IFormattable
     {
-        //TODO
+        /// <summary>
+        /// Simple auto properties for parameters of book.
+        /// </summary>
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public string PublishingHouse { get; set; }
+        public int Edition { get; set; }
+        public int Pages { get; set; }
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// Overriden method ToString();
+        /// </summary>
+        /// <returns>
+        /// String representation of book.
+        /// </returns>
+        public override string ToString()
+        {
+            return ToString("G", CultureInfo.CurrentCulture);
+        }
+
+        public string ToString(string format)
+        {
+            return ToString(format, CultureInfo.CurrentCulture);
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            if (string.IsNullOrWhiteSpace(format))
+            {
+                format = "G";
+            }
+
+            if (formatProvider == null)
+            {
+                formatProvider = CultureInfo.CurrentCulture;
+            }
+
+            switch (format.ToUpper())
+            {
+                case "G":
+                case "A":
+                    return $"Book record: {Author}, {Title}, {Year}, {PublishingHouse}";
+                case "B":
+                    return $"Book record: {Author}, {Title}, {Year}";
+                case "C":
+                    return $"Book record: {Author}, {Title}";
+                case "D":
+                    return $"Book record: {Title}, {Year}, {PublishingHouse}";
+                case "E":
+                    return $"Book record: {Title}, {PublishingHouse}";
+                case "F":
+                    return $"Book record: {Author}, {Title}, {Year}, {PublishingHouse}, ed.{Edition}, p.{Pages}, {Price:C2}";
+                default:
+                    throw new FormatException(nameof(format) + " is invalid format.");
+            }
+        }
     }
 }
